352cc46f0e315a3ec568d9dc7437dda1c413aee4
Nicholson Koukpaizan <koukpaizannk@ornl.gov>
2025-04-03 13:50:57 -0400

    Make gridkit package explicitly depend on LLVM when +enzyme.

--- var/spack/repos/builtin/packages/gridkit/package.py
+++ var/spack/repos/builtin/packages/gridkit/package.py
@@ -26,6 +26,7 @@ class Gridkit(CMakePackage):
     depends_on("c", type="build")
     depends_on("cxx", type="build")
     depends_on("enzyme", when="+enzyme")
+    depends_on("llvm", when="+enzyme")
     depends_on("ipopt~mumps+coinhsl", when="+ipopt")
     depends_on("sundials@7:+klu~mpi", when="+sundials+klu")
     depends_on("sundials@7:~klu~mpi", when="+sundials~klu")
